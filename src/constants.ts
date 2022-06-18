02887a86ac71eb91f3a0d0239b42bf441ae6b642
Lidcer <thelidcer@gmail.com>
2022-06-18 16:04:41 +0200

    Fix typo

--- src/constants.ts
+++ src/constants.ts
@@ -6,14 +6,14 @@ interface Score {
 export const LINES_SCORES: Score[] = [
     { name: "Single", value: 100},
     { name: "Double", value: 300},
-    { name: "Tripe" , value: 500},
+    { name: "Triple" , value: 500},
     { name: "Quad"  , value: 800}
 ];
 
 export const SPIN_BONUS: Score[] = [
     { name: "-spin Single", value: 100 },
     { name: "-spin Double", value: 300 },
-    { name: "-spin Tripe" , value: 500 },
+    { name: "-spin Triple" , value: 500 },
     { name: "-spin Quad"  , value: 800 }
 ];
 export const CLEAR_ALL = 3500;
